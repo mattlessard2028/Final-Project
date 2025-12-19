5db438eabba4017f2b4f66e8ce6336ff1fd347e0
mattlessard2028 <matthew.lessard1@snhu.edu>
2025-12-19 17:39:07 -0500

    Add files via upload

--- views.py
+++ views.py
@@ -1,42 +1,6 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
-from .models import Article
-from django.views.generic import TemplateView
+from django.http import HttpResponse
 
-class HomeView(TemplateView):
-    template_name = "home.html"
-
-# Article views
-class ArticleListView(ListView):
-    model = Article
-    template_name = "articles/article_list.html"  # include "articles/" if inside that folder
+def home(request):
+    return HttpResponse("Hello, world!")
 
 
-class ArticleDetailView(DetailView):
-    model = Article
-    template_name = "articles/article_detail.html"
-
-
-class ArticleCreateView(CreateView):
-    model = Article
-    template_name = "articles/article_new.html"
-    fields = ("title", "body", "author")
-
-
-class ArticleUpdateView(UpdateView):
-    model = Article
-    template_name = "articles/article_edit.html"
-    fields = ("title", "body")
-
-
-class ArticleDeleteView(DeleteView):
-    model = Article
-    template_name = "articles/article_delete.html"
-    success_url = reverse_lazy("article_list")
-
-
-
-
-# Create your views here.
